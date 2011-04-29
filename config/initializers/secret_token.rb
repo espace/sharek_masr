43d62d63bac710aed19d617121a635c551501aba
rofaida <rofaida@rofaida.(none)>
2011-04-29 12:14:59 +0300

    All project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharek::Application.config.secret_token = 'a341117fd2ea0a749c27eac2a7eda503d0be1fd3dfe131a3f5bdd0513989cdb788a7faf4bb4cb259b1cf4b7e539cea3f76cf2ccadd1d296adab921817a95426f'
