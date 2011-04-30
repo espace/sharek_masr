e6c8f79209acef9997826a7802c907674a1ce3b9
Ahmad Bakr <ahmad.bakr@espace.com.eg>
2011-04-30 16:11:53 +0300

    needs

--- db/migrate/20110430123740_fill_data.rb
+++ db/migrate/20110430123740_fill_data.rb
@@ -2,16 +2,16 @@
 class FillData < ActiveRecord::Migration
   def self.up
     10.times.each do |t|
-    Need.create(:title=>"اكفل طفل يتيم #{t}",:descriptionn=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>false, :city_id=>City.first.id, :category_id=>Category.find_by_name("رعاية الأيتام").id, :district_id=> District.first.id, :address=> "قريه مصريه", :urgency_id=>Urgency.last.id, :organisation_id=>Organisation.find_by_name("رسالة"))
+    Need.create(:title=>"اكفل طفل يتيم #{t}",:description=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>false, :city_id=>City.first.id, :category_id=>Category.find_by_name("رعاية الأيتام").id, :district_id=> District.first.id, :address=> "قريه مصريه", :urgency_id=>Urgency.last.id, :organisation_id=>Organisation.find_by_name("رسالة"))
     end
 
 
     10.times.each do |t|
-    Need.create(:title=>"اكفل طفل يتيم #{t}",:descriptionn=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>true, :city_id=>City.first.id, :category_id=>Category.find_by_name("اطفال الشوارع").id, :district_id=> District.first.id, :address=> "قريه مصريه", :urgency_id=>Urgency.last.id, :organisation_id=>Organisation.find_by_name("رسالة"))
+    Need.create(:title=>"اكفل طفل يتيم #{t}",:description=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>true, :city_id=>City.first.id, :category_id=>Category.find_by_name("اطفال الشوارع").id, :district_id=> District.first.id, :address=> "قريه مصريه", :urgency_id=>Urgency.last.id, :organisation_id=>Organisation.find_by_name("رسالة"))
     end
 
     10.times.each do|t|
-    Need.create(:title=>"اكفل طفل يتيم #{t}",:descriptionn=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>true, :city_id=>City.first.id, :category_id=>Category.find_by_name("اطفال الشوارع").id, :district_id=> District.last.id, :address=> "قريه مصريه", :urgency_id=>Urgency.first.id, :organisation_id=>Organisation.find_by_name("رسالة"))
+    Need.create(:title=>"اكفل طفل يتيم #{t}",:description=>"طفل يتيم يعيش فى قريه مصريه بسيطه فقد والدة", :verified=>true, :satisfied=>true, :city_id=>City.first.id, :category_id=>Category.find_by_name("اطفال الشوارع").id, :district_id=> District.last.id, :address=> "قريه مصريه", :urgency_id=>Urgency.first.id, :organisation_id=>Organisation.find_by_name("رسالة"))
     end
   end
 
