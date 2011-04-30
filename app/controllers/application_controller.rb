85834ef2c2bd6b0d7c07186b635cd3b63bc66366
Ahmad Bakr <ahmad.bakr@espace.com.eg>
2011-04-30 17:29:27 +0300

    design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :preparation
+
+  def preparation
+    @categories = Category.all
+    @urgencies = Urgency.all
+    @districts = District.all
+
+  end 
 end
